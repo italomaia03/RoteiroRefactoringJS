4e7a2e4baed36f19bc68780e1fa71fdef2b37053
italomaia03 <italo.fernandes.om@gmail.com>
2024-02-03 21:43:39 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -3,19 +3,30 @@ const { readFileSync } = require("fs");
 function gerarFaturaStr(fatura, pecas) {
   let totalFatura = 0;
   let creditos = 0;
+  function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+      minimumFractionDigits: 2,
+    }).format(valor / 100);
+  }
   let faturaStr = `Fatura ${fatura.cliente}\n`;
-  const formato = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2,
-  }).format;
 
   function getPeca(apresentacao) {
     return pecas[apresentacao.id];
   }
 
   for (let apre of fatura.apresentacoes) {
-    const peca = getPeca(apre);
+    creditos += calcularCredito(apre);
+
+    function calcularCredito(apre) {
+      let creditos = 0;
+      // créditos para próximas contratações
+      creditos += Math.max(apre.audiencia - 30, 0);
+      if (getPeca(apre).tipo === "comedia")
+        creditos += Math.floor(apre.audiencia / 5);
+      return creditos;
+    }
 
     function calcularTotalApresentacao(apre) {
       let total = 0;
@@ -41,17 +52,13 @@ function gerarFaturaStr(fatura, pecas) {
 
     let total = calcularTotalApresentacao(apre);
 
-    // créditos para próximas contratações
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia") creditos += Math.floor(apre.audiencia / 5);
-
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${
-      apre.audiencia
-    } assentos)\n`;
+    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(
+      calcularTotalApresentacao(apre)
+    )} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
-  faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
+  faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
   faturaStr += `Créditos acumulados: ${creditos} \n`;
   return faturaStr;
 }
